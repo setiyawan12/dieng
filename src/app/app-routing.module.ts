ae24d8255deb947c0b7825d401cfc03ac3da669d
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-09-18 16:08:05 +0700

    kesel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'setting', loadChildren: './setting/setting.module#SettingPageModule' },
   { path: 'customer', loadChildren: './customer/customer.module#CustomerPageModule' },
-  { path: 'addcustomer', loadChildren: './addcustomer/addcustomer.module#AddcustomerPageModule' },
-  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
-  { path: 'pengaduan', loadChildren: './pages/pengaduan/pengaduan.module#PengaduanPageModule' },
   { path: 'pengaturan', loadChildren: './pages/pengaturan/pengaturan.module#PengaturanPageModule' },
 ];
 
